aa0adf571aa92446c1ebf192235e2a720ba395aa
BulBul Ahmed <mdbulbulmolla1222@gmail.com>
2023-02-16 10:51:57 +0600

    fix some issue, change layout, added product price fuctionality

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ document.getElementById("remove-cart-section").addEventListener("click", () => {
   document.body.style.overflowY = "scroll";
 });
 
+
+
 //! add to cart section functionality
 
 const parentDiv = document.getElementsByClassName("product-list-side");
@@ -19,16 +21,77 @@ const parentDiv = document.getElementsByClassName("product-list-side");
 for (const item of parentDiv) {
   item.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
+
+      //! fetch product image
       document
         .getElementById("cart-image-div")
         .appendChild(e.target.parentNode.previousSibling.previousSibling);
+
+
+      //! fetch product name
       document.getElementById("cart-product-item-name").innerText =
         e.target.parentNode.childNodes[3].innerText;
-      document.getElementById("cart-item-price").innerText =
-        e.target.parentNode.childNodes[7].children[0].childNodes[1].data;
-      console.log(
-        e.target.parentNode.childNodes[7].children[0].childNodes[1].data
+
+
+
+      //! fetch product price
+      const productPrice =
+        e.target.parentNode.childNodes[7].children[0].childNodes[2].data;
+      document.getElementById("cart-item-price").innerText = productPrice;
+
+
+      //! input field
+      const cartInputField = document.getElementById(
+        "cart-product-countity-input"
       );
+      let cartInputFirstValue = 1;
+      cartInputField.value = cartInputFirstValue;
+
+
+
+      //! subTotal & total fields
+
+      const subTotalField = document.getElementById("product-subtotal");
+      const cartTotalSubtotal = document.getElementById("cart-total-subtotal");
+      const totalPriceTax = document.getElementById("total-price-tax");
+
+      subTotalField.innerText = parseInt(productPrice) * cartInputFirstValue;
+      console.log(cartTotalSubtotal.innerText);
+      cartTotalSubtotal.innerText = parseInt(productPrice) * cartInputFirstValue;
+      totalPriceTax.innerText = (parseInt(productPrice) * cartInputFirstValue * 0.1).toFixed(2);
+
+
+      //! increment functionality
+      const incrementButton = document.getElementById("incrementButton");
+
+      incrementButton.addEventListener("click", () => {
+        decrementButton.disabled = false;
+
+        cartInputField.value++;
+        let totalPrice = cartInputField.value * parseInt(productPrice);
+        subTotalField.innerText = totalPrice;
+        cartTotalSubtotal.innerText = totalPrice;
+
+        totalPriceTax.innerText = (parseInt(cartTotalSubtotal.innerText) * 0.1).toFixed(2);
+      });
+
+
+
+      //! decrement functionality
+
+      const decrementButton = document.getElementById("decrementButton");
+
+      decrementButton.addEventListener("click", () => {
+        if (cartInputField.value <= 1) {
+          decrementButton.disabled = true;
+        } else {
+          cartInputField.value--;
+          let totalPrice = cartInputField.value * parseInt(productPrice);
+          subTotalField.innerText = totalPrice;
+          cartTotalSubtotal.innerText = totalPrice;
+          totalPriceTax.innerText = (parseInt(cartTotalSubtotal.innerText) * 0.1).toFixed(2);
+        }
+      });
     }
   });
 }
